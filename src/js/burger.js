a4296d4fb4deb17cda7eb789cb0d5ea21355cf0d
Kateryna Khanenko <mala.lototska@gmail.com>
2019-10-10 16:13:42 +0300

    menu added

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,6 +1,8 @@
 const $burger = $('.js-burger');
 const $body = $('body');
+const $menu = $('.js-menu');
 
 $burger.on('click', function() {
   $body.toggleClass('is-menu-open');
+  $menu.toggleClass('menu-visible');
 });
