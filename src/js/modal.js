90edddbdd46de21ec589eaf535d1c649465d6346
Kateryna Khanenko <mala.lototska@gmail.com>
2019-10-22 16:40:32 +0300

    cookies js-close

--- src/js/modal.js
+++ src/js/modal.js
@@ -2,6 +2,10 @@ const $modalCall = $('.js-modal-call');
 const $body = $('body');
 const $modalClose = $('.js-modal-close');
 
+const $cookiesClose = $('.js-cookies-close');
+const $cookies = $('.js-cookies');
+
+
 $modalCall.on('click', function() {
   let targetModal = $(this).data('modal');
   $body.toggleClass('is-modal-open');
@@ -12,3 +16,8 @@ $modalClose.on('click', function() {
   $body.removeClass('is-modal-open');
   $('.modal.is-open').removeClass('is-open');
 });
+
+$cookiesClose.on('click', function() {
+  $cookies.addClass('modal--cookies');
+});
+
