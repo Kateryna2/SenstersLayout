d7c5fffe61bb80a1d6a576a0f1c4e5a4a6b4e798
Kateryna Khanenko <mala.lototska@gmail.com>
2019-10-25 14:30:05 +0300

    scroll on first page - stops on first enter

--- src/js/scroll-events.js
+++ src/js/scroll-events.js
@@ -9,26 +9,53 @@ const $textInfoblock = $('.js-text-infoblock');
 //   }
 // });
 
-$infoBlock.on('wheel', function() {
-  //   let scrollingHeight = $(this).find('.info-block--inside').height() + 155;
-  //   let currentScrollPosition = $(this).scrollTop() + $(this).height();
-  // // console.log(
-  // //   ($(this).find('.info-block--inside').height() + 155),
-  // //   ($(this).scrollTop() + $(this).height())
-  // // );
+$infoBlock.on('wheel', function () {
+    //   let scrollingHeight = $(this).find('.info-block--inside').height() + 155;
+    //   let currentScrollPosition = $(this).scrollTop() + $(this).height();
+    // // console.log(
+    // //   ($(this).find('.info-block--inside').height() + 155),
+    // //   ($(this).scrollTop() + $(this).height())
+    // // );
 
-    $textInfoblock.each(function() {
+    $textInfoblock.each(function () {
         let $this = $(this);
         let thisPosTop = $this.position().top;
         let thisHref = $this.attr('id');
 
-        if(thisPosTop >= 0 && thisPosTop < 10) {
+
+        if (thisPosTop >= 0 && thisPosTop < 10) {
             $(`.js-info-block-number[href="#${thisHref}"]`).addClass('is-active');
             $(`.js-info-block-number[href="#${thisHref}"]`).siblings().removeClass('is-active');
         }
+
+
     });
 
+
 });
+
+
+$(window).on('scroll touchmove mousewheel', function () {
+
+    if ($(window).scrollTop() >= $('.quote').outerHeight() + $('.banner').outerHeight() ) {
+        $body.css({
+            'overflow': 'hidden',
+            'height': '100%',
+            'width': '100%'
+        });
+
+        if ($( '#end' ).hasClass('is-active') || $( '#begin' ).hasClass('is-active')) {
+            $body.css({
+                'overflow': 'auto',
+                'height': 'auto',
+                'width': 'auto'
+            });
+        }
+        }
+
+
+});
+
 
 // function infoBlockAnimation() {
 //   let posTop = $infoBlock[0].getBoundingClientRect().top - 118;
