123ec8a09c5a7b2d32cf6a73c71175954ce71338
Max Novikov <max.vashuk@gmail.com>
2019-11-19 12:24:46 +0200

    fixes

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,4 +1,7 @@
-$('.js-reviews-list').slick({
+const $reviewsList = $('.js-reviews-list');
+const $reviewsListNav = $('.js-reviews-list-nav');
+
+$reviewsList.slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     centerMode: true,
@@ -10,7 +13,8 @@ $('.js-reviews-list').slick({
     speed: 1000,
     asNavFor: '.js-reviews-list-nav'
 });
-$('.js-reviews-list-nav').slick({
+
+$reviewsListNav.slick({
     slidesToShow: 3,
     slidesToScroll: 1,
     asNavFor: '.js-reviews-list',
@@ -22,40 +26,6 @@ $('.js-reviews-list-nav').slick({
     speed: 1000,
     fade: false
 });
-
-
-
-// const $slider = $('.info-block--inside');
-// $slider
-//     .on('init', () => {
-//         mouseWheel($slider);
-//     })
-//     .slick({
-//         slidesToShow: 1,
-//         slidesToScroll: 1,
-//         dots: false,
-//         vertical: true,
-//         verticalScrolling: true,
-//         infinite: false,
-//         arrows: false,
-//         speed: 1000
-//     });
-//
-// function mouseWheel($slider) {
-//     $($slider).on('wheel', {$slider: $slider}, mouseWheelHandler);
-// }
-//
-// function mouseWheelHandler(event) {
-//     event.preventDefault();
-//     const $slider = event.data.$slider;
-//     const delta = event.originalEvent.deltaY;
-//     if (delta > 0) {
-//         $slider.slick('slickNext');
-//     } else {
-//         $slider.slick('slickPrev');
-//     }
-// }
-
 
 
 
